56de7a4046ebf8d3c4e0e251449b73112a3abf26
boivlad <boyvlad99@gmail.com>
2020-05-12 22:49:45 +0300

    optimize algorithm, add README.md

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-const antennaCoverage = function(n, m, antennasData) {
+const antennaCoverageCost = function(n, m, antennasData) {
   const antennas = [];
   for (let i = 0; i < antennasData.length; i += 2) {
     antennas.push({
@@ -8,55 +8,54 @@ const antennaCoverage = function(n, m, antennasData) {
   }
 
   const coverage = getCoverage(m, antennas);
-  const toCover = m - coverage.reduce((count, isCovered) => count + isCovered, 0);
-  if (!toCover) {
+  const left = m - coverage.reduce((count, isCovered) => count + isCovered, 0);
+  if (!left) {
     return 0;
   }
-  let isCovered = false;
-  let cost = Math.floor(toCover / 2);
-  while (!isCovered) {
-    const combs = getCombinations(cost, n);
-    for (let comb of combs) {
+  let unCovered = true;
+  let cost = Math.floor(left / 2);
+  while (unCovered) {
+    const combinations = getCombinations(cost, n);
+    for (let combination of combinations) {
       const ants = antennas.map((item, i) => ({
         x: item.x,
-        s: item.s + comb[i]
+        s: item.s + combination[i]
       }));
       if (!getCoverage(m, ants).includes(0)) {
-        isCovered = true;
+        unCovered = false;
         break;
       }
     }
-    if (!isCovered) {
+    if (unCovered) {
       cost += 1;
     }
   }
   return cost;
 }
 const getCoverage = function(m, antennas) {
-  const result = Array(m).fill(0);
+  const dp = Array(m).fill(0);
   for (let i = 0; i < antennas.length; i++) {
-    const { x, s } = antennas[i];
-    const from = Math.max(0, x - s);
-    const to = Math.min(m - 1, x + s);
-    for (let j = from; j <= to; j++) {
-      result[j] = 1;
+    const min = Math.max(0, antennas[i].x - antennas[i].s);
+    const max = Math.min(m - 1, antennas[i].x + antennas[i].s);
+    for (let j = min; j <= max; j++) {
+      dp[j] = 1;
     }
   }
-  return result;
+  return dp;
 }
 
 function getCombinations(n, m) {
-  const combs = [];
+  const combinations = [];
   if (m === 2) {
     for (let i = 0; i <= n; i++) {
-      combs.push([i, n - i]);
+      combinations.push([i, n - i]);
     }
   } else {
     for (let i = 0; i <= n; i++) {
-      getCombinations(n - i, m - 1).forEach(a => combs.push([i, ...a]));
+      getCombinations(n - i, m - 1).forEach(item => combinations.push([i, ...item]));
     }
   }
-  return combs;
+  return combinations;
 }
 
-module.exports = antennaCoverage;
+module.exports = antennaCoverageCost;
--- test/test.example.js
+++ test/test.example.js
@@ -1,13 +1,13 @@
 const assert = require('assert');
 const { performance } = require('perf_hooks');
-const antennaCoverage = require('../src/index');
+const antennaCoverageCost = require('../src/index');
 
 describe('Test basic case', function() {
   let sumTime = 0;
   it('1) common antennas count 3, coverage 595, antennas array [43, 2, 300, 4, 554, 10]',
     function() {
       let start = performance.now()
-      const result = antennaCoverage(3, 595, [43, 2, 300, 4, 554, 10]);
+      const result = antennaCoverageCost(3, 595, [43, 2, 300, 4, 554, 10]);
       let end = performance.now()
       sumTime += end - start
       assert.strictEqual(result, 281);
@@ -15,7 +15,7 @@ describe('Test basic case', function() {
   it('2) common antennas count 1, coverage 1, antennas array [1, 1, 1, 1]',
     function() {
       let start = performance.now()
-      const result = antennaCoverage(1, 1, [1, 1, 1, 1]);
+      const result = antennaCoverageCost(1, 1, [1, 1, 1, 1]);
       let end = performance.now()
       sumTime += end - start
       assert.strictEqual(result, 0);
@@ -23,7 +23,7 @@ describe('Test basic case', function() {
   it('3) common antennas count 2, coverage 50, antennas array [20, 0, 3, 1]',
     function() {
       let start = performance.now()
-      const result = antennaCoverage(2, 50, [20, 0, 3, 1]);
+      const result = antennaCoverageCost(2, 50, [20, 0, 3, 1]);
       let end = performance.now()
       sumTime += end - start
       assert.strictEqual(result, 30);
@@ -32,7 +32,7 @@ describe('Test basic case', function() {
     '4) common antennas count 5, coverage 240, antennas array [13, 0, 50, 25, 60, 5, 155, 70, 165, 70]',
     function() {
       let start = performance.now()
-      const result = antennaCoverage(5, 240, [13, 0, 50, 25, 60, 5, 155, 70, 165, 70]);
+      const result = antennaCoverageCost(5, 240, [13, 0, 50, 25, 60, 5, 155, 70, 165, 70]);
       let end = performance.now()
       sumTime += end - start
       assert.strictEqual(result, 26);
