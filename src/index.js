1c627cb03e0b14a87db717d516a74d104db1b76a
boivlad <boyvlad99@gmail.com>
2020-05-12 22:26:24 +0300

    completed task

--- src/index.js
+++ src/index.js
@@ -1,13 +1,9 @@
 const antennaCoverage = function(n, m, antennasData) {
-  const antennas = [];
-  for (let i = 0; i < antennasData.length; i += 2) {
-    antennas.push({
-      x: antennasData[i] - 1,
-      s: antennasData[i + 1],
-    })
-  }
+  // for (let i = 0; i < antennasData.length; i += 2) {
+  //   antennasData[i] -= 1;
+  // }
 
-  const coverage = getCoverage(m, antennas);
+  const coverage = getCoverage(m, antennasData);
   const toCover = m - coverage.reduce((count, isCovered) => count + isCovered, 0);
   if (!toCover) {
     return 0;
@@ -17,11 +13,10 @@ const antennaCoverage = function(n, m, antennasData) {
   while (!isCovered) {
     const combs = getCombinations(cost, n);
     for (let comb of combs) {
-      const ants = antennas.map((item, i) => ({
-        x: item.x,
-        s: item.s + comb[i]
-      }));
-      if (!getCoverage(m, ants).includes(0)) {
+      for (let i = 0; i < antennasData.length; i += 2) {
+        antennasData[i + 1] += comb[i];
+      }
+      if (!getCoverage(m, antennasData).includes(0)) {
         isCovered = true;
         break;
       }
@@ -33,9 +28,11 @@ const antennaCoverage = function(n, m, antennasData) {
   return cost;
 }
 const getCoverage = function(m, antennas) {
+
   const result = Array(m).fill(0);
-  for (let i = 0; i < antennas.length; i++) {
-    const { x, s } = antennas[i];
+  for (let i = 0; i < antennas.length; i += 2) {
+    const x = antennas[i];
+    const s = antennas[i + 1];
     const from = Math.max(0, x - s);
     const to = Math.min(m - 1, x + s);
     for (let j = from; j <= to; j++) {
